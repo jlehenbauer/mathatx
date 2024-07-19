24add226e14e4838e48ba66b10f3d93657743ffc
jlehenbauer <jake@lehenbauer.us>
2024-07-19 17:38:27 -0500

    Eliminate secrets

--- AliciaWeb/settings.py
+++ AliciaWeb/settings.py
@@ -104,11 +104,11 @@ AUTH_PASSWORD_VALIDATORS = [
 #gmail_send
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'jake.lehenbauer@gmail.com'
-EMAIL_HOST_PASSWORD = 'ofiu tooq sern ftjs'
+EMAIL_HOST_USER = '##'
+EMAIL_HOST_PASSWORD = '################'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = 'jake.lehenbauer@gmail.com'
+DEFAULT_FROM_EMAIL = '##'
 
 
 # Internationalization
